48bf08f971f77d5d175b2c00c2edcb47433af7cc
yura-prots <yuraprots@gmail.com>
2024-01-09 12:16:52 +0200

    Profile component successfully implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Profile } from "./Profile/Profile";
+import user from '../db/user.json'
 
 export const App = () => {
   return (
@@ -12,7 +13,13 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      <Profile />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,43 @@
-export const Profile = () => {
+import PropTypes from "prop-types";
+
+export const Profile = ({username, tag, location, avatar, stats}) => {
     return (<div class="profile">
     <div class="description">
       <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-        alt="User avatar"
+        src={avatar}
+        alt={username}
         class="avatar"
       />
-      <p class="name">Petra Marica</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
+      <p class="name">{username}</p>
+      <p class="tag">{tag}</p>
+      <p class="location">{location}</p>
     </div>
   
     <ul class="stats">
       <li>
         <span class="label">Followers</span>
-        <span class="quantity">1000</span>
+        <span class="quantity">{stats.followers}</span>
       </li>
       <li>
         <span class="label">Views</span>
-        <span class="quantity">2000</span>
+        <span class="quantity">{stats.views}</span>
       </li>
       <li>
         <span class="label">Likes</span>
-        <span class="quantity">3000</span>
+        <span class="quantity">{stats.likes}</span>
       </li>
     </ul>
   </div>)
+}
+
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.exact({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired
+    })
 }
