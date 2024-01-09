2d7874692d82749fc3c4e2f3d61fba2157ab8a30
yura-prots <yuraprots@gmail.com>
2024-01-09 22:02:54 +0200

    delete readme files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,16 +11,7 @@ import transactions from '../db/transactions.json'
 
 export const App = () => {
   return (
-    <div
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   color: '#010101'
-      // }}
-    >
+    <div>
       <Profile
         username={user.username}
         tag={user.tag}
