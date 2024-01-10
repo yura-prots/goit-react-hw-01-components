4cf75d539113f8973ed37b703055fa5879d4c890
yura-prots <yuraprots@gmail.com>
2024-01-10 21:59:12 +0200

    emotion library installed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Profile } from 'components/Profile/Profile';
-import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+// import { Statistics } from 'components/Statistics/Statistics';
+// import { FriendList } from 'components/FriendList/FriendList';
+// import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
 import user from '../db/user.json';
-import data from '../db/data.json';
-import friends from '../db/friends.json';
-import transactions from '../db/transactions.json';
+// import data from '../db/data.json';
+// import friends from '../db/friends.json';
+// import transactions from '../db/transactions.json';
 
 export const App = () => {
   return (
@@ -19,12 +19,12 @@ export const App = () => {
         stats={user.stats}
       />
 
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
+      {/* <Statistics title="Upload stats" stats={data} /> */}
+      {/* <Statistics stats={data} /> */}
 
-      <FriendList friends={friends} />
+      {/* <FriendList friends={friends} /> */}
 
-      <TransactionHistory items={transactions} />
+      {/* <TransactionHistory items={transactions} /> */}
     </div>
   );
 };
