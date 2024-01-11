c1595acd7ebb085d31adf113025caae00b30888c
yura-prots <yuraprots@gmail.com>
2024-01-11 15:35:15 +0200

    add GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { GlobalStyles } from 'GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>
+
 );
